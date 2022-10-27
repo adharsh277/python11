f0f3582a9300e607fa66b0e2eff4c23165cdab97
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-10-27 19:45:54 +0530

    Add files via upload

---
+++ pythonday111.py
@@ -0,0 +1,38 @@
+
+# regional chunni
+x = "so from the beging they were asking why people fall in love when they find a girls,its because they aren't the same viceversa"
+y = "he was good, but he dont know whats going to happen because he kow some ting wierd is going to happen, the ami reasonn was fear"
+print(x.upper())
+print(y.lower())
+print(x.lower())
+print(y.upper())
+print(x.replace("beging", 'beginning'))
+print(x.split())
+print(y.split())
+print(y.replace("girls", "womens"))
+
+print(type(x))
+print(type(y))
+print(x.replace("e", "o"))
+print(y.replace("r", "u"))
+print(y.replace("k", "p"))
+print(y.replace("h", "k"))
+print(y.replace("l", "r"))
+print(y.replace("y", "t"))
+print(y.replace("a", "q"))
+print(y.replace("t", "c"))
+print(y.replace("d", "k"))
+print(y.replace("k", "t"))
+print(x.split())
+print(x.split())
+print(300 == 56)
+print(562456+62672)
+print(26573-5623)
+print(7627-789065)
+print(7627*789065)
+print('lionel messi')
+print("cristano ronaldo")
+print("richard alexandra andrews", 'gona gana reddy', 'rimbochee',
+      "rehensh pillauri", 'kothamangalm dhamu', "dinkendshwera")
+print(x)
+# python operators
