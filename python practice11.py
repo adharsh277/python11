ef80c4e1671923af662a88e6cf2bec1bc1f37d80
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-10-27 14:59:51 +0530

    Add files via upload

---
+++ python practice11.py
@@ -0,0 +1,51 @@
+from calendar import c
+from re import A
+from tkinter import Y
+
+
+x = 30
+y = 67
+z = x+y
+print(z)
+A = "ranikabeer is a naughty girl from the country side whom cannot stand at any cost so what we can do is just be the best to others"
+B = "if lionel mmessi argentina can show others that they are best in their own way they can be the perfect team n atar fifa "
+c = "your cordinates for the earth will be the "
+d = 98393, 94763, 1346, 7389, 2345
+print(A+B+c)
+print(d)
+myname = 'john samuel umitti is not goona good at you'
+print(myname+"but richard is  gonna create so ething you  can't feel")
+x, y = 90, 765
+print(x*22)
+print(x*9)
+print(x/55)
+# now it s for modifictaion
+x = "we are creating new world with only peoples wish that should be meta version"
+print(x[-1:])
+print(x[1:])
+print(x[-9:-9])
+print(x[-9:8])
+print(x[9:-1])
+print(x.upper())
+print(x.lower())
+print(x.split())
+y = "You just have to decide what kind of man you want to grow up to be humans Whoever that man is he's going to change the world "
+print(x.upper())
+print(x.lower())
+print(x.split())
+print(x.replace('e', 'y'))
+print(x[1:])
+# now its time for the booleans
+print(bool(False))
+print(bool(True))
+# arithemetic operator
+print(10+381+67+80+817)
+print(10+38+67+874)
+print(10+67+80+87)
+print(138+675+80+585)
+# just my creativ
+print(123 == 345)
+print(723*8)
+print(736**78)
+print(234//928)
+print(8930/67)
